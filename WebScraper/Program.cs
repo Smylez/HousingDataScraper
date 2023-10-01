4edb7a991c94843ae4c85fe7f2be2f48cb4f2b7d
Smylez <flip13@gmail.com>
2023-09-30 21:03:12 -0300

    Code cleanup, correcting posting date pull

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -10,70 +10,29 @@ using OfficeOpenXml.Table;
 using static System.Net.WebRequestMethods;
 using System.Resources;
 using WebScraper;
+using System.Threading.Tasks;
 
 class Program
 {
     static async System.Threading.Tasks.Task Main(string[] args)
     {
+        //TODO - move these to where they're actually used instead of all at the top
         string filePath = "C:\\Users\\Flip\\Downloads";
-        string locationInput;
-        string locationKey;
-        string categoryInput;
-        string categoryKey;
-        string roomsInput;
-        int numberOfRooms;
         string baseURL = "https://www.kijiji.ca";
-        //Filtering to long term rentals only for now
-        string filteredUrl = baseURL + "/b-apartments-condos/";
         string cardUrl;
         bool isFiltered;
-        string html;
-        string detailsHTML;
-        string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
-        string price;
-        string location;
-        string postingDate;
-        string address;
-        string listingDetailsURL;
-        string listingContainerNodeString = "//ul[@data-testid='srp-search-list']";
-        string listingCardNodeString = ".//li[starts-with(@data-testid, 'listing-card-list-item-')]";
-        //string listingCardLinkString = ".//a[@data-testid='listing-link']";
-        string priceNodeString = ".//p[@data-testid='listing-price']";
-        string locationNodeString = ".//p[@data-testid='listing-location']";
-        string postingDateNodeString = ".//p[@data-testid='listing-date']";
-        string listingDetailsURLNodeString = ".//a[@data-testid='listing-link']";
-        string addressNodeString = ".//span[@itemprop='address']";
         int pageScrapeDelaytimer = 1000;
-        int adDetailsScrapeDelaytimer = 500;
-        string startMarker = "datetime=\"";
-        string endMarker = "Z\" title";
-        int startIndex;
-        int endIndex;
-        int datetimeLength;
 
         Console.WriteLine("Please enter the city:");
-        locationInput = Console.ReadLine();
-        locationKey = Locations.ResourceManager.GetString(locationInput.ToUpper());
-        ////Hardcoding these values for now, not sure how Kijiji generates them but they're location specific
-        //if (locationInput.ToUpper() == "FREDERICTON")
-        //{
-        //    locationKey = "c37l1700018";
-        //}
-        //if (locationInput.ToUpper() == "MONCTON")
-        //{
-        //    locationKey = "c37l1700001";
-        //}
-        //else
-        //{
-        //    locationKey = "";
-        //}
+        string locationInput = Console.ReadLine();
+        string locationKey = Locations.ResourceManager.GetString(locationInput.ToUpper());
 
         //TODO - put all this in GUI 
         //keep in mind some locations are displayed oddly (ie: New Brunswick is new-brunswick)
 
         Console.WriteLine("Please the number of bedrooms between 1 & 4, enter 0 for bachelor/studio:");
-        roomsInput = Console.ReadLine();
-        if(!int.TryParse(roomsInput, out numberOfRooms))
+        string roomsInput = Console.ReadLine();
+        if(!int.TryParse(roomsInput, out int numberOfRooms))
         {
             Console.WriteLine("Input is not valid. Please enter a number.");
             throw new Exception("Input is not valid. Please enter a number.");
@@ -84,11 +43,14 @@ class Program
             throw new Exception("Input is not valid. Please choose a number between 0 & 4");
         }
 
-        categoryInput = "RealEstate";
-        categoryKey = Categories.ResourceManager.GetString(categoryInput.ToUpper());
+        string categoryInput = "RealEstate";
+        string categoryKey = Categories.ResourceManager.GetString(categoryInput.ToUpper());
 
         //TODO - this needs update since the location key changes
         //don't need a27949001 unless filtering
+        //Filtering to long term rentals only for now
+        string filteredUrl = baseURL + "/b-apartments-condos/";
+
         if (numberOfRooms == 0) {
             roomsInput = "bachelor+studio";
             filteredUrl = filteredUrl + locationInput + "/" + roomsInput + "/c" + categoryKey + "l" + locationKey + "a27949001?ad=offer";//"a27949001";//"/c37l1700018a27949001";
@@ -105,19 +67,23 @@ class Program
             try
             {
                 //Fake the user agent
+                string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";          
                 client.DefaultRequestHeaders.Add("User-Agent", userAgent);
- 
+                
+                ///pagination start
                 //Get the html
                 //only gets first page, need to find a way to know how many pages of results are there in advance
-                html = await client.GetStringAsync(filteredUrl);
+                string html = await client.GetStringAsync(filteredUrl);
                 var doc = new HtmlDocument();
                 doc.LoadHtml(html);
 
                 //Find the main container that holds all the listing cards.
+                string listingContainerNodeString = "//ul[@data-testid='srp-search-list']";              
                 var listingContainer = doc.DocumentNode.SelectSingleNode(listingContainerNodeString);
                 if (listingContainer != null)
                 {
                     //Find all the listing cards within the container.
+                    string listingCardNodeString = ".//li[starts-with(@data-testid, 'listing-card-list-item-')]";
                     var listingCards = listingContainer.SelectNodes(listingCardNodeString);
 
                     if (listingCards != null)
@@ -128,39 +94,62 @@ class Program
                         foreach (var card in listingCards)
                         {
                             //Extract listing price and location.
+                            string priceNodeString = ".//p[@data-testid='listing-price']";
                             var priceNode = card.SelectSingleNode(priceNodeString);
+                            string locationNodeString = ".//p[@data-testid='listing-location']";
                             var locationNode = card.SelectSingleNode(locationNodeString);
-                            price = priceNode?.InnerText.Trim() ?? "N/A";
-                            location = locationNode?.InnerText.Trim() ?? "N/A";
+                            string price = priceNode?.InnerText.Trim() ?? "N/A";
+                            string location = locationNode?.InnerText.Trim() ?? "N/A";
 
                             //Link to details page
+                            string listingDetailsURLNodeString = ".//a[@data-testid='listing-link']";
                             var listingDetailsURLNode = card.SelectSingleNode(listingDetailsURLNodeString);
-                            listingDetailsURL = baseURL + listingDetailsURLNode?.GetAttributeValue("href", string.Empty);
-                                                       
-                            //TODO - navigate to each card to get address info (do we need to rate limit?)
-                            //var cardURL = card.SelectSingleNode(listingCardLinkString);
-                            //add delay here
-                            detailsHTML = await client.GetStringAsync(listingDetailsURL);
+                            string listingDetailsURL = baseURL + listingDetailsURLNode?.GetAttributeValue("href", string.Empty);
+
+                            //Navigate to each card to get address info
+                            //Add 500 ms delay here to prevent ban detection
+                            await Task.Delay(500);
+                            string detailsHTML = await client.GetStringAsync(listingDetailsURL);
                             var detailsDoc = new HtmlDocument();
                             detailsDoc.LoadHtml(detailsHTML);
 
                             //Get address and posting date
                             var detailsContainer = detailsDoc.DocumentNode.SelectSingleNode("//div[@id='mainPageContent']");
-                            var addressNode = detailsContainer.SelectSingleNode("//div[contains(@class, 'locationContainer-')]//span[@itemprop='address']");
-                            var postingDateNode = detailsContainer.SelectSingleNode("//div[contains(@class, 'datePosted-')]");
-                            address = addressNode?.InnerText.Trim() ?? "N/A";
+                            string addressNodeString = "//div[contains(@class, 'locationContainer-')]//span[@itemprop='address']";
+                            var addressNode = detailsContainer.SelectSingleNode(addressNodeString);
+                            string postingDateNodeString = "//div[contains(@class, 'datePosted-')]";
+                            var postingDateNode = detailsContainer.SelectSingleNode(postingDateNodeString);
+                            string address = addressNode?.InnerText.Trim() ?? "N/A";
+
+                            //Alternate markers
+                            //string startMarker = "datetime=\"";
+                            //string endMarker = "Z\" title";
 
                             //Posting date substring manipulation
-                            startIndex = postingDateNode.InnerHtml.IndexOf(startMarker);
-                            //Get endof index position to start
-                            startIndex = startIndex + startMarker.Length;
-                            //Get positon to end the substring
-                            endIndex = postingDateNode.InnerHtml.IndexOf(endMarker, startIndex);
-                            // Calculate the length of the datetime string to return
-                            datetimeLength = endIndex - startIndex;
-                            // Extract the datetime value
-                            postingDate = postingDateNode?.InnerHtml.Substring(startIndex, datetimeLength).Trim() ?? "N/A";
-
+                            string startMarker = "title=\"";
+                            string endMarker = "\">";
+                            string postingDate = "N/A";
+                            //detects ads that have been taken down recently, this can cause an exception
+                            if (postingDateNode != null)
+                            {
+                                int startIndex = postingDateNode.InnerHtml.IndexOf(startMarker);
+                                //Get endof index position to start
+                                startIndex = startIndex + startMarker.Length;
+                                //Get positon to end the substring
+                                int endIndex = postingDateNode.InnerHtml.IndexOf(endMarker, startIndex);
+                                // Calculate the length of the datetime string to return
+                                int datetimeLength = endIndex - startIndex;
+                                
+                                if (datetimeLength > 0)
+                                {
+                                    // Extract the datetime value
+                                    postingDate = postingDateNode?.InnerHtml.Substring(startIndex, datetimeLength).Trim() ?? "N/A";
+                                }
+                            }
+                            else
+                            {
+                                postingDate = "Taken Down";
+                            }
                             scrapedData.Add((price, location, postingDate, listingDetailsURL, address));
                         }
                         //TODO - add pagination before export call, add 1000ms delay before each page scrape to mimic reasonable user input
@@ -183,6 +172,8 @@ class Program
                 {
                     Console.WriteLine("No listing container found on the page.");
                 }
+                ///pagination end
+                ///put export here instead after pagination working. That will collect all the records before exporting
             }
             catch (HttpRequestException ex)
             {
@@ -197,9 +188,9 @@ class Program
         {
             var worksheet = package.Workbook.Worksheets.Add("Scraped Data");
             worksheet.Cells["A2"].Value = "Location";
-            worksheet.Cells["B2"].Value = "Price";
-            worksheet.Cells["C2"].Value = "Posting Date";
-            worksheet.Cells["D2"].Value = "Address";
+            worksheet.Cells["B2"].Value = "Address";
+            worksheet.Cells["C2"].Value = "Price";
+            worksheet.Cells["D2"].Value = "Posting Date";
 
             int row = 3;
             
@@ -210,21 +201,28 @@ class Program
             //titleCell.Style.Fill.BackgroundColor.SetColor(blue);
 
             //TODO: should I filter out sponsored ads?
-            // const filteredAds = allAds.filter(ad => ad.adSource === "ORGANIC");
-            // Filter out rows where the price is "Please Contact"
+            //const filteredAds = allAds.filter(ad => ad.adSource === "ORGANIC");
+            //Filter out rows where the price is "Please Contact"
             data.RemoveAll(item => item.price.Equals("Please Contact", StringComparison.OrdinalIgnoreCase));
-            
+            //Filter out useless records
+            data.RemoveAll(item => item.address.Equals("N/A", StringComparison.OrdinalIgnoreCase));
+
             foreach (var item in data)
             {
                 worksheet.Cells[row, 1].Value = item.location;
-                worksheet.Cells[row, 2].Style.Numberformat.Format = "$#,##0.00";
+                worksheet.Cells[row, 2].Value = item.address;
+               
                 if (decimal.TryParse(item.price, System.Globalization.NumberStyles.Currency, null, out decimal parsedPrice))
                 {
-                    worksheet.Cells[row, 2].Value = parsedPrice;
+                    worksheet.Cells[row, 3].Value = parsedPrice;
                 }
-                //worksheet.Cells[row, 2].Value = decimal.TryParse(item.Price, System.Globalization.NumberStyles.Currency, null, out decimal result) ? result : item.Price;
-                worksheet.Cells[row, 3].Value = item.postingDate;
-                worksheet.Cells[row, 3].Value = item.address;
+                worksheet.Cells[row, 3].Style.Numberformat.Format = "$#,##0.00";
+
+                if (DateTime.TryParse(item.postingDate, out DateTime parsedDate))
+                {
+                    worksheet.Cells[row, 4].Value = parsedDate;
+                }
+                worksheet.Cells[row, 4].Style.Numberformat.Format = "yyyy-MM-dd";
 
                 row++;
             }
@@ -237,7 +235,7 @@ class Program
             worksheet.Cells[row, 2].Style.Numberformat.Format = "$#,##0.00";
 
             //Format the table.
-            var tableRange = worksheet.Cells["A2:B" + row];
+            var tableRange = worksheet.Cells["A2:D" + row];
             var table = worksheet.Tables.Add(tableRange, "ScrapedDataTable");
             table.TableStyle = TableStyles.Light16;
 
