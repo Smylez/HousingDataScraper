ccb092f1fa4252c16d93a72b90623c13ff7801c3
Smylez <flip13@gmail.com>
2023-10-02 20:59:53 -0300

    Adding GUI

---
+++ WebScraper/WebScraperGUI.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WebScraper
+{
+    partial class WebScraperGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebScraperGUI));
+            this.goButton = new System.Windows.Forms.Button();
+            this.roomsFilterCombobox = new System.Windows.Forms.ComboBox();
+            this.locationCombobox = new System.Windows.Forms.ComboBox();
+            this.Disclaimer = new System.Windows.Forms.Label();
+            this.loadingGif = new System.Windows.Forms.PictureBox();
+            this.loadingLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingGif)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goButton.Location = new System.Drawing.Point(100, 283);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(400, 66);
+            this.goButton.TabIndex = 0;
+            this.goButton.Text = "GO";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // roomsFilterCombobox
+            // 
+            this.roomsFilterCombobox.FormattingEnabled = true;
+            this.roomsFilterCombobox.Location = new System.Drawing.Point(200, 245);
+            this.roomsFilterCombobox.Name = "roomsFilterCombobox";
+            this.roomsFilterCombobox.Size = new System.Drawing.Size(200, 21);
+            this.roomsFilterCombobox.TabIndex = 1;
+            this.roomsFilterCombobox.Text = "Number Of Rooms";
+            this.roomsFilterCombobox.SelectedIndexChanged += new System.EventHandler(this.roomsFilterCombobox_SelectedIndexChanged);
+            // 
+            // locationCombobox
+            // 
+            this.locationCombobox.FormattingEnabled = true;
+            this.locationCombobox.Location = new System.Drawing.Point(200, 208);
+            this.locationCombobox.Name = "locationCombobox";
+            this.locationCombobox.Size = new System.Drawing.Size(200, 21);
+            this.locationCombobox.TabIndex = 3;
+            this.locationCombobox.Text = "Location";
+            this.locationCombobox.SelectedIndexChanged += new System.EventHandler(this.locationComboBox_SelectedIndexChanged);
+            // 
+            // Disclaimer
+            // 
+            this.Disclaimer.AutoSize = true;
+            this.Disclaimer.Font = new System.Drawing.Font("Segoe UI", 12.25F);
+            this.Disclaimer.Location = new System.Drawing.Point(27, 9);
+            this.Disclaimer.Name = "Disclaimer";
+            this.Disclaimer.Size = new System.Drawing.Size(533, 115);
+            this.Disclaimer.TabIndex = 5;
+            this.Disclaimer.Text = resources.GetString("Disclaimer.Text");
+            // 
+            // loadingGif
+            // 
+            this.loadingGif.Image = ((System.Drawing.Image)(resources.GetObject("loadingGif.Image")));
+            this.loadingGif.Location = new System.Drawing.Point(200, 146);
+            this.loadingGif.Name = "loadingGif";
+            this.loadingGif.Size = new System.Drawing.Size(222, 203);
+            this.loadingGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.loadingGif.TabIndex = 6;
+            this.loadingGif.TabStop = false;
+            this.loadingGif.Click += new System.EventHandler(this.loadingGif_Click);
+            // 
+            // loadingLabel
+            // 
+            this.loadingLabel.AutoSize = true;
+            this.loadingLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadingLabel.Location = new System.Drawing.Point(270, 118);
+            this.loadingLabel.Name = "loadingLabel";
+            this.loadingLabel.Size = new System.Drawing.Size(80, 25);
+            this.loadingLabel.TabIndex = 7;
+            this.loadingLabel.Text = "Loading";
+            // 
+            // WebScraperGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.loadingGif);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.roomsFilterCombobox);
+            this.Controls.Add(this.locationCombobox);
+            this.Controls.Add(this.Disclaimer);
+            this.Controls.Add(this.loadingLabel);
+            this.Name = "WebScraperGUI";
+            this.Text = "Housing Data Scraper";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loadingGif)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ComboBox roomsFilterCombobox;
+        private System.Windows.Forms.ComboBox locationCombobox;
+        private System.Windows.Forms.Label Disclaimer;
+        private System.Windows.Forms.PictureBox loadingGif;
+        private System.Windows.Forms.Label loadingLabel;
+    }
+}
---
+++ WebScraper/WebScraperGUI.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace WebScraper
+{
+    public partial class WebScraperGUI : Form
+    {
+        public string locationInput;
+        public string locationKey;
+        public string roomsFilter;
+        public string filePath = "C:\\Users\\Flip\\Downloads";
+        public WebScraperGUI()
+        {
+            InitializeComponent();
+            locationCombobox.Validating += locationCombobox_Validating;
+            goButton.Enabled = false;
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+            var resourceItems = new List<Location>();
+            ResourceManager resourceManager = new ResourceManager("Webscraper.Locations", typeof(WebScraperGUI).Assembly);
+            var resourceSet = resourceManager.GetResourceSet(System.Globalization.CultureInfo.CurrentUICulture, true, true);
+
+            // Add a default item at the beginning of the list
+            resourceItems.Add(new Location
+            {
+                DisplayText = "Select a location...",
+                Value = string.Empty
+            });
+
+            if (resourceSet != null)
+            {
+                foreach (DictionaryEntry entry in resourceSet)
+                {
+                    var resourceItem = new Location
+                    {
+                        Value = entry.Value.ToString(),
+                        DisplayText = entry.Key.ToString()
+                    };
+                    resourceItems.Add(resourceItem);
+                }
+            }
+            // Bind the ComboBox to the list of ResourceItems
+            locationCombobox.DataSource = resourceItems;
+            locationCombobox.DisplayMember = "DisplayText";
+            locationCombobox.ValueMember = "Value";
+
+            roomsFilterCombobox.Items.AddRange(new string[] { "N/A", "Bachelor", "1", "2", "3", "4" });
+
+            loadingGif.Visible = false;
+            loadingLabel.Visible = false;
+        }
+
+        private void locationCombobox_Validating(object sender, CancelEventArgs e)
+        {
+            // Check if the default item is selected
+            if (locationCombobox.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select a location.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Set e.Cancel to true to prevent focus from leaving the ComboBox
+                e.Cancel = true;
+            }
+            else
+            {     
+                goButton.Enabled = true;
+            }
+        }
+
+        private void locationComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            locationInput = locationCombobox.Text.ToString();
+            locationKey = locationCombobox.SelectedValue.ToString();
+        }
+
+        private void roomsFilterCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            roomsFilter = roomsFilterCombobox.SelectedItem.ToString();
+        }
+
+        private async void goButton_Click(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+
+                    loadingLabel.Text = "Processing...";
+                    loadingGif.Visible = true;
+                    loadingLabel.Visible = true;
+                    // Disable the Go button to prevent multiple clicks during processing
+                    goButton.Enabled = false;
+
+                    // Call the web scraping method and pass any necessary parameters
+                    await Task.Run(() => Program.RunWebScraper(new string[] { locationInput, locationKey, roomsFilter, filePath }));
+                    //await Program.RunWebScraper(new string[] { locationInput, locationKey, roomsFilter, filePath });
+
+                    loadingGif.Visible = false;
+                    loadingLabel.Text = "Scraping completed!";
+                }
+                catch (Exception ex)
+                {
+                }
+                finally
+                {
+                    goButton.Enabled = true;
+                }
+            }
+        }
+
+        private void loadingGif_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
